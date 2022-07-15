8787504de4d4117920f4472d0ce7d1481d29093e
Dany Doerr <mail@danieldoerr.de>
2022-07-15 09:51:00 +0200

    canonize edge representation

--- src/core.rs
+++ src/core.rs
@@ -1,3 +1,7 @@
+/* crate use */
+use rustc_hash::FxHashMap;
+
+
 pub const MASK_LEN: u64 = 1073741823;
 pub const BITS_NODEID: u8 = 64 - MASK_LEN.count_ones() as u8;
 
@@ -7,10 +11,10 @@ pub trait Countable: Sized + Copy {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Hash, Eq, Ord)]
-pub struct Node(pub u64);
+pub struct Node(u64);
 
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Hash, Eq, Ord)]
-pub struct Edge(pub u64);
+pub struct Edge(u64);
 
 impl Countable for Node {
     fn hash(self) -> u64 {
@@ -69,33 +73,52 @@ impl Edge {
             id2,
             u32::MAX - u32::pow(2, 31)
         );
-        let mut hash = (id1 << 32) + id2;
-        if is_reverse1 {
+
+        let (uid, u_is_reverse, vid, v_is_reverse) = Edge::canonize(id1, is_reverse1, id2, is_reverse2);
+        
+        let mut hash = (uid << 32) + vid;
+        if u_is_reverse {
             hash += u64::pow(2, 63);
         }
-        if is_reverse2 {
+        if v_is_reverse {
             hash += u64::pow(2, 31);
         }
         Self(hash)
     }
 
-    pub fn id1(self) -> u64 {
+    fn canonize(id1: u64, is_reverse1: bool, id2: u64, is_reverse2: bool) -> (u64, bool, u64, bool) {
+        if (is_reverse1 && is_reverse2) || (is_reverse1 != is_reverse2 && id1 > id2) {
+            (id2, !is_reverse2, id1, !is_reverse1)
+        } else {
+            (id1, is_reverse1, id2, is_reverse2)
+        }
+    }
+
+    pub fn uid(self) -> u64 {
         (self.0 >> 32) & (u32::MAX - u32::pow(2, 31)) as u64
     }
 
-    pub fn is_reverse1(self) -> bool {
+    pub fn u_is_reverse(self) -> bool {
         (self.0 & u64::pow(2, 63)) > 0
     }
 
-    pub fn id2(self) -> u64 {
+    pub fn vid(self) -> u64 {
         self.0 & (u32::MAX as u64 - 2 ^ 32)
     }
 
-    pub fn is_reverse2(self) -> bool {
+    pub fn v_is_reverse(self) -> bool {
         (self.0 & u64::pow(2, 31)) > 0
     }
 
     pub fn hash(self) -> u64 {
         Countable::hash(self)
+    }
+}
+
+pub struct Abacus<T: Countable>(Vec<T>);
+
+impl Abacus<Node>{
+    fn from_gfa<R: std::io::Read>(mut data: &std::io::BufReader<R>) {
+        
     }
 }
