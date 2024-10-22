dd63670f8ac63b1c3d94140feb83e355da29b6b7
Dany Doerr <daniel.doerr@hhu.de>
2024-10-22 15:05:14 +0200

    add symlink to panacus-visualize in bin

--- package.sh
+++ package.sh
@@ -12,6 +12,7 @@ VERSION="0.2.5"
 ARCH="${PANACUS_TARGET}"
 # ARCH="x86_64-unknown-linux-musl"
 # ARCH="x86_64-apple-darwin"
+# ARCH="aarch64-apple-darwin"
 
 echo "Packaging ${NAME}, ${EXEC}, ${VERSION}, ${ARCH}"
 cargo fmt && \
@@ -42,6 +43,7 @@ cp -r ./docs ./pkg/${NAME}-${VERSION}_${ARCH}/
 mkdir ./pkg/${NAME}-${VERSION}_${ARCH}/bin
 cp target/${ARCH}/release/${EXEC} ./pkg/${NAME}-${VERSION}_${ARCH}/bin/
 strip ./pkg/${NAME}-${VERSION}_${ARCH}/bin/${EXEC}
+ln -s ../scripts/$NAME-visualize.py ./pkg/${NAME}-${VERSION}_${ARCH}/$NAME-visualize
 
 cp LICENSE ./pkg/${NAME}-${VERSION}_${ARCH}/
 cp README.md ./pkg/${NAME}-${VERSION}_${ARCH}/
